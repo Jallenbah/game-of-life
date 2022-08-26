b26a773926212aad581ae51c76050d54c9d7ceea
Jallen <111014323+Jallenbah@users.noreply.github.com>
2022-08-26 22:25:36 +0100

    Removed/renamed pixelwindow sample code

---
+++ src/App/GameOfLifeAppManager.cs
@@ -0,0 +1,22 @@
+﻿using PixelWindowSystem;
+using SFML.Graphics;
+
+class GameOfLifeAppManager : IPixelWindowAppManager
+{
+    public void OnLoad(RenderWindow renderWindow)
+    {
+    }
+
+    public void Update(float frameTime)
+    {
+    }
+
+    public void FixedUpdate(float timeStep)
+    {
+    }
+
+    public void Render(PixelData pixelData, float frameTime)
+    {
+        pixelData.Clear();
+    }
+}
--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,46 +1,10 @@
 ﻿using PixelWindowSystem;
-using SFML.Graphics;
 
-var appManager = new RandomPixelsAppManager();
+const uint width = 256, height = 144, scale = 4;
 
-var window = new PixelWindow(1024, 576, 8, "Big pixels", appManager,
-    fixedTimestep: 20, framerateLimit: 300);
+var appManager = new GameOfLifeAppManager();
+
+var window = new PixelWindow(width * scale, height * scale, scale, "Game of life", appManager,
+    fixedTimestep: 100, framerateLimit: 200);
 
 window.Run();
-
-// All we need to do is implement the methods in IPixelWindowAppManager and pass in an instance of this class to the
-// PixelWindow constructor, as above.
-class RandomPixelsAppManager : IPixelWindowAppManager
-{
-    private Random _rand = new();
-
-    public void OnLoad(RenderWindow renderWindow)
-    {
-        // On load function - runs once at start.
-        // The SFML render window provides ability to set up events and input (maybe store a reference to it for later use in your update functions)
-    }
-
-    public void Update(float frameTime)
-    {
-        // Update function - process update logic to run every frame here
-    }
-
-    public void FixedUpdate(float timeStep)
-    {
-        // Fixed update function - process logic to run every fixed timestep here
-    }
-
-    public void Render(PixelData pixelData, float frameTime)
-    {
-        // Render function - set pixel data for the current frame here
-        // Randomised pixels shown as example.
-        pixelData.Clear();
-        for (uint x = 0; x < pixelData.Width; x++)
-        {
-            for (uint y = 0; y < pixelData.Height; y++)
-            {
-                pixelData[x, y] = ((byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255));
-            }
-        }
-    }
-}
